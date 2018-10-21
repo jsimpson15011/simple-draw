ea358932bcdb5879ed6c08073234fe2966cb1a54
jsimpson15011 <joseph.franklin.simpson@gmail.com>
2018-10-20 23:09:22 -0600

    Add fill and draw change ico

--- src/App.js
+++ src/App.js
@@ -1,185 +1,289 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import './App.css';
 import Button from './Button.js';
 import Controls from './Controls';
 
-const history=[];
-let colorIndex=0;
-let historyIndex=-1;
-let cleared=true;
+const history = [];
+let colorIndex = 0;
+let historyIndex = -1;
+let cleared = true;
+
 class App extends Component {
-  constructor(props){
-    let canvasWidth=15;
-    let canvasSize=Math.pow(canvasWidth,2);
-    super(props);
-    this.state={
-      canvasSize: canvasSize,
-        colorSelector:'black',
-        canvas: Array(canvasSize*canvasSize).fill('white')
-    };
-    this.handleClick = this.handleClick.bind(this);
-  }
-
-  handleClick(i){
-      if (history[historyIndex+1]){
-          history[historyIndex+1]=this.state.canvas;
-          history.length=historyIndex+2;
-      }
-      else {
-          history.push(this.state.canvas);
-      }
-      if(cleared===true){
-        cleared=false;
-      }
-    historyIndex+=1;
-    let newState=this.state.canvas.slice();
-        newState[i]=this.state.colorSelector;
-/*        newState[i+1]=this.state.colorSelector;
-      newState[i+15]=this.state.colorSelector;
-      newState[i+16]=this.state.colorSelector;*/
-    this.setState(
-        {canvas: newState}
-    );
-  }
-
-  colorSelect(){
-    let newColor='';
-    switch (colorIndex){
-        case 0:
-          newColor='red';
-          break;
-        case 1:
-          newColor='orange';
-            break;
-        case 2:
-            newColor='yellow';
-            break;
-        case 3:
-            newColor='green';
-            break;
-        case 4:
-            newColor='blue';
-            break;
-        case 5:
-            newColor='purple';
-            break;
-        case 6:
-            newColor='white';
-            break;
-        case 7:
-            newColor='black';
-            break;
-        default:
-          newColor='black';
-          colorIndex=0;
-          break;
+    constructor(props) {
+        let canvasWidth = 15;
+        let canvasSize = Math.pow(canvasWidth, 2);
+        super(props);
+        this.state = {
+            canvasSize: canvasSize,
+            colorSelector: 'black',
+            canvas: Array(canvasSize * canvasSize).fill('white'),
+            tool: 'draw',
+            drawBackground:'#4e6890',
+            fillBackground:'',
+            undoBackground:'#909090',
+            redoBackground:'#909090'
+        };
+        this.handleClick = this.handleClick.bind(this);
     }
-    if (colorIndex<7){
-      ++colorIndex;
+    draw(){
+        this.setState(
+            {tool: 'draw'}
+        );
+        this.setState(
+            {drawBackground:'#4e6890'}
+        );
+        this.setState(
+            {fillBackground:''}
+        );
     }
-    else {
-      colorIndex=0;
+    fill(){
+        this.setState(
+            {tool: 'fill'}
+        );
+        this.setState(
+            {fillBackground:'#4e6890'}
+        );
+        this.setState(
+            {drawBackground:''}
+        );
     }
-      this.setState(
-          {colorSelector: newColor}
-      )
-  }
-  undo(){
-    if (historyIndex!==-1){
-        if (history[historyIndex+1]){
-            history[historyIndex+1]=this.state.canvas;
+    handleClick(i) {
+        let newState = this.state.canvas.slice();
+        if (history[historyIndex + 1]) {
+            history[historyIndex + 1] = this.state.canvas;
+            history.length = historyIndex + 2;
         }
         else {
             history.push(this.state.canvas);
         }
+        if (cleared === true) {
+            cleared = false;
+        }
+        if (!history[historyIndex + 2]) {
+            this.setState(
+                {redoBackground:'#909090'}
+            );
+        }
+        historyIndex += 1;
         this.setState(
-            {canvas:history[historyIndex]}
-        )
-        historyIndex-=1
-        if (cleared===true){
-          cleared=false;
+            {undoBackground:''}
+        );
+        switch (this.state.tool){
+            case 'draw':
+                newState[i] = this.state.colorSelector;
+                /*        newState[i+1]=this.state.colorSelector;
+                newState[i+15]=this.state.colorSelector;
+                newState[i+16]=this.state.colorSelector;*/
+                this.setState(
+                    {canvas: newState}
+                );
+                break;
+            case 'fill':
+                newState[i+2] = this.state.colorSelector;
+                this.setState(
+                    {canvas: newState}
+                );
+                break;
+            default:
+                this.setState(
+                    {tool: 'draw'}
+                );
+                break;
         }
     }
-  }
-  redo(){
-    if (history[historyIndex+2]){
-        historyIndex+=1
-      this.setState(
-          {canvas:history[historyIndex+1]}
-      )
+
+    colorSelect() {
+        let newColor = '';
+        switch (colorIndex) {
+            case 0:
+                newColor = 'red';
+                break;
+            case 1:
+                newColor = 'orange';
+                break;
+            case 2:
+                newColor = 'yellow';
+                break;
+            case 3:
+                newColor = 'green';
+                break;
+            case 4:
+                newColor = 'blue';
+                break;
+            case 5:
+                newColor = 'purple';
+                break;
+            case 6:
+                newColor = 'white';
+                break;
+            case 7:
+                newColor = 'black';
+                break;
+            default:
+                newColor = 'black';
+                colorIndex = 0;
+                break;
+        }
+        if (colorIndex < 7) {
+            ++colorIndex;
+        }
+        else {
+            colorIndex = 0;
+        }
+        this.setState(
+            {colorSelector: newColor}
+        )
     }
-  }
-  clear(){
-    if (cleared===false){
-      cleared=true;
-        if (history[historyIndex+1]){
-            history[historyIndex+1]=this.state.canvas;
-            history.length=historyIndex+2;
+
+    undo() {
+        if (historyIndex !== -1) {
+            if (history[historyIndex + 1]) {
+                history[historyIndex + 1] = this.state.canvas;
+            }
+            else {
+                history.push(this.state.canvas);
+            }
+            this.setState(
+                {canvas: history[historyIndex]}
+            )
+            historyIndex -= 1
+            if (cleared === true) {
+                cleared = false;
+            }
+            this.setState(
+                {redoBackground:''}
+            );
+        }
+        if(historyIndex===-1){
+            this.setState(
+                {undoBackground:'#909090'}
+            );
+        }
+    }
+
+    redo() {
+        if (history[historyIndex + 2]) {
+            historyIndex += 1
+            this.setState(
+                {canvas: history[historyIndex + 1]}
+            )
+            this.setState(
+                {undoBackground:''}
+            )
+        }
+        if (!history[historyIndex + 2]) {
+            this.setState(
+                {redoBackground:'#909090'}
+            );
+        }
+    }
+
+    clear() {
+        if (cleared === false) {
+            cleared = true;
+            if (history[historyIndex + 1]) {
+                history[historyIndex + 1] = this.state.canvas;
+                history.length = historyIndex + 2;
+            }
+            else {
+                history.push(this.state.canvas);
+            }
+            if (!history[historyIndex + 2]) {
+                this.setState(
+                    {redoBackground:'#909090'}
+                );
+            }
+            historyIndex += 1;
+            let newState = Array(this.state.canvasSize * this.state.canvasSize).fill('white');
+            this.setState(
+                {canvas: newState}
+            )
+        }
+    }
+
+    fillAll() {
+        if (history[historyIndex + 1]) {
+            history[historyIndex + 1] = this.state.canvas;
+            history.length = historyIndex + 2;
         }
         else {
             history.push(this.state.canvas);
         }
-        historyIndex+=1;
-        let newState=Array(this.state.canvasSize*this.state.canvasSize).fill('white');
+        if (!history[historyIndex + 2]) {
+            this.setState(
+                {redoBackground:'#909090'}
+            );
+        }
+        historyIndex += 1;
+        if (cleared === true) {
+            cleared = false;
+        }
+        let newState = Array(this.state.canvasSize * this.state.canvasSize).fill(this.state.colorSelector);
         this.setState(
-            {canvas:newState}
+            {canvas: newState}
         )
     }
-  }
-  fill(){
-      if (history[historyIndex+1]){
-          history[historyIndex+1]=this.state.canvas;
-          history.length=historyIndex+2;
-      }
-      else {
-          history.push(this.state.canvas);
-      }
-      historyIndex+=1;
-      if (cleared===true){
-        cleared=false;
-      }
-      let newState=Array(this.state.canvasSize*this.state.canvasSize).fill(this.state.colorSelector);
-      this.setState(
-          {canvas:newState}
-      )
-  }
-  renderButtons(i){
-    let canvas=[]
-      let buttons=[]
-      for (let i = 0; i < this.state.canvasSize; i++){
-          buttons.push(<Button
-              className={'canvas-cell'}
-              key={i}
-              onClick={() => {this.handleClick(i)}}
-              color={'white'}
-              background={this.state.canvas[i]}
-              width={(100/Math.sqrt(this.state.canvasSize))+'%'}
-              height={(100/Math.sqrt(this.state.canvasSize))+'%'}
-              text={this.state.canvas[i]}
-          />)
-      }
-      canvas.push(buttons);
-    return canvas;
-  }
-  render() {
 
-    return (
-      <div className="App">
-        <div className="canvas">
-            <h2 style={{width:"100%",textAlign:"center",color:"white"}}>SIMPLE ART</h2>
-            {this.renderButtons()}
-            <Controls
-                background={this.state.colorSelector}
-                onClick={() => {this.colorSelect()}}
-                onUndo={() => {this.undo()}}
-                onRedo={() => {this.redo()}}
-                onClear={() => {this.clear()}}
-                onFill={() => {this.fill()}}
-            />
-        </div>
-      </div>
-    );
-  }
+    renderButtons(i) {
+        let canvas = []
+        let buttons = []
+        for (let i = 0; i < this.state.canvasSize; i++) {
+            buttons.push(<Button
+                className={'canvas-cell'}
+                key={i}
+                onClick={() => {
+                    this.handleClick(i)
+                }}
+                color={'white'}
+                background={this.state.canvas[i]}
+                width={(100 / Math.sqrt(this.state.canvasSize)) + '%'}
+                height={(100 / Math.sqrt(this.state.canvasSize)) + '%'}
+                text={this.state.canvas[i]}
+            />)
+        }
+        canvas.push(buttons);
+        return canvas;
+    }
+
+    render() {
+
+        return (
+            <div className="App">
+                <div className="canvas">
+                    <h2 style={{width: "100%", textAlign: "center", color: "white"}}>SIMPLE ART</h2>
+                    {this.renderButtons()}
+                    <Controls
+                        background={this.state.colorSelector}
+                        onClick={() => {
+                            this.colorSelect()
+                        }}
+                        onUndo={() => {
+                            this.undo()
+                        }}
+                        onRedo={() => {
+                            this.redo()
+                        }}
+                        onClear={() => {
+                            this.clear()
+                        }}
+                        onFillAll={() => {
+                            this.fillAll()
+                        }}
+                        onDraw={() => {
+                            this.draw()
+                        }}
+                        drawBackground={this.state.drawBackground}
+                        fillBackground={this.state.fillBackground}
+                        undoBackground={this.state.undoBackground}
+                        redoBackground={this.state.redoBackground}
+                        onFill={() => {
+                            this.fill()
+                        }}
+                    />
+                </div>
+            </div>
+        );
+    }
 }
 
 export default App;
--- src/Controls.js
+++ src/Controls.js
@@ -1,26 +1,56 @@
 import React, { Component } from 'react';
-
+let pencilIconName= require('./draw-icon.png');
+let bucketIconName= require('./bucket-icon.png');
 class Controls extends Component {
     render() {
         return (
             <div className={'color-selector-container'}>
-                <h2 style={{textAlign:'center',color:'white'}}>Color Selector:</h2>
-                <button className={'color-selector'}
-                        onClick={this.props.onClick}
-                        style={{
-                            background: this.props.background,
-                            color: this.props.color
-                        }}>
-                </button>
+                <div className={'upper-control-container'}>
+                    <button className={'blue-button draw'}
+                            id={'draw'}
+                            onClick={this.props.onDraw}
+                            style={{
+                                color: 'white',
+                                backgroundColor:this.props.drawBackground
+                            }}>
+                        <img alt={""} src={pencilIconName}/>
+                        DRAW
+                    </button>
+                    <div>
+                        <h2 style={{textAlign:'center',color:'white'}}>Color Selector:</h2>
+                        <button className={'color-selector'}
+                                id={'colorSelector'}
+                                onClick={this.props.onClick}
+                                style={{
+                                    background: this.props.background,
+                                    color: this.props.color
+                                }}>
+                        </button>
+                    </div>
+                    <button className={'blue-button draw'}
+                            id={'fill'}
+                            onClick={this.props.onFill}
+                            style={{
+                                color: 'white',
+                                backgroundColor:this.props.fillBackground
+                            }}>
+                        <img alt={""} src={bucketIconName}/>
+                        FILL
+                    </button>
+                </div>
                 <div className={'undo-redo-container'}>
-                    <button className={'undo'}
+                    <button
+                            id={'undo'}
+                            className={'blue-button undo'}
                             onClick={this.props.onUndo}
                             style={{
-                                color: 'white'
+                                color: 'white',
+                                backgroundColor:this.props.undoBackground
                             }}>
                         UNDO
                     </button>
                     <button className={'red-button'}
+                            id={'clear'}
                             onClick={this.props.onClear}
                             style={{
                                 color: 'white'
@@ -28,16 +58,19 @@ class Controls extends Component {
                         CLEAR ALL
                     </button>
                     <button className={'red-button'}
-                            onClick={this.props.onFill}
+                            id={'fillAll'}
+                            onClick={this.props.onFillAll}
                             style={{
                                 color: 'white'
                             }}>
                         FILL ALL
                     </button>
-                    <button className={'redo'}
+                    <button className={'blue-button redo'}
+                            id={'redo'}
                             onClick={this.props.onRedo}
                             style={{
-                                color: 'white'
+                                color: 'white',
+                                backgroundColor:this.props.redoBackground
                             }}>
                         REDO
                     </button>
